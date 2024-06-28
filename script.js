2f24ec9d6e6bb13f523abefc9e5267743c7e2aac
Gabriel Leung <gabby.t.leung@gmail.com>
2024-06-28 15:14:38 +0800

    Deleted redundant code

--- script.js
+++ script.js
@@ -12,18 +12,3 @@ let minutes = now.getMinutes();
 let seconds = now.getSeconds();
 
 document.getElementById("bg-time").textContent = `${hours}:${minutes}:${seconds}`;
-
-(function (d, s, id) {
-    if (d.getElementById(id)) {
-        if (window.__TOMORROW__) {
-            window.__TOMORROW__.renderWidget();
-        }
-        return;
-    }
-    const fjs = d.getElementsByTagName(s)[0];
-    const js = d.createElement(s);
-    js.id = id;
-    js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
-
-    fjs.parentNode.insertBefore(js, fjs);
-})(document, 'script', 'tomorrow-sdk');
