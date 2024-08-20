4c14622cc31291d10bf7d9da15c66e18797d0d79
Gabriel Leung <gabby.t.leung@gmail.com>
2024-08-21 01:26:50 +0800

    added updateTime function

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,4 @@
 $(() => {
-  const index = {};
-
   main.intersectFadeInOut("#projects", 0.3);
 
   main.intersect("#content-buffer", 0, entry => {
@@ -16,9 +14,31 @@ $(() => {
         .querySelectorAll("#content #fixed")
         .forEach(entry => entry.classList.remove("fixed"));
   });
+
+  main.intersect("#contacts", 0.3, entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
 });
 
-// buttons
+const index = {};
+
+index.updateTime = () => {
+  const now = new Date();
+  const options = {
+    timeZone: "America/New_York",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  const timeString = now.toLocaleString("en-US", options);
+  document.getElementById("time-content").textContent = timeString;
+};
+
+setInterval(index.updateTime, 1000);
+
+// BUTTONS
 function jumbotronButtonScroll() {
   window.scrollBy(0, window.innerHeight / 3);
 }
